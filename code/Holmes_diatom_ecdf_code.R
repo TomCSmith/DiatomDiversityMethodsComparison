786f35dd57249973afb0eac80c33f235f7983dc4
Mark Wilber <mqwilber@gmail.com>
2014-06-13 20:45:17 -0700

    Made CDF plots...not so pretty

--- code/Holmes_diatom_ecdf_code.R
+++ code/Holmes_diatom_ecdf_code.R
@@ -1,25 +1,31 @@
 library(vegan)
 
-#load data: holmesdiatomsabundbylake.csv
-Diatoms<-read.csv(file.choose(), header=T)
-str(Diatoms)
-DiatomMatrix<-as.matrix(Diatoms[,-1])
-str(DiatomMatrix)
+### Exploratory analysis of Holmes data...looking at the ecdfs ###
 
-#eCDF - drop zeros, plot remaining species
-v<-NULL
-for(i in 1:dim(DiatomMatrix)[1]){
-	tvec<-DiatomMatrix[i,]
-	ind<-tvec!=0
-	nozeroesvec<-tvec[ind]
-	if(i==1) {plot(ecdf(nozeroesvec))}
-	else {lines(ecdf(nozeroesvec))}
-	}
+# Read in Holmes data...I think this is Windows compatible
+Diatoms <- read.csv(file.path("..", "data", "archival", "HolmesDiatomsAbundByLake.csv"),
+                    header=T)
 
-for(i in 1:1){
-	tvec<-DiatomMatrix[i,]
-	ind<-tvec!=0
-	nozeroesvec<-tvec[ind]
-	if(i==1) {plot(ecdf(nozeroesvec))}
-	else {lines(ecdf(nozeroesvec))}
-	}
+DiatomMatrix <- as.matrix(Diatoms[,-1])
+S_vec = array(NA, )
+
+# eCDF - drop zeros, plot remaining species
+for(i in 1:dim(DiatomMatrix)[1]) {
+
+    # Drop zeros from SAD
+	tvec <- DiatomMatrix[i,]
+	ind <- tvec != 0
+	nozeroesvec <- tvec[ind]
+    nozeroesvec = sort(nozeroesvec)
+
+    ecdf_fxn = ecdf(nozeroesvec)
+	if(i==1) {
+        plot(log(nozeroesvec), ecdf_fxn(nozeroesvec), type="b", pch=21, col=i,
+                    xlab="log(Abundance)", ylab="CDF")
+    }
+	else {
+        lines(log(nozeroesvec), ecdf_fxn(nozeroesvec), type="b", pch=21, col=i)
+    }
+}
+
+
